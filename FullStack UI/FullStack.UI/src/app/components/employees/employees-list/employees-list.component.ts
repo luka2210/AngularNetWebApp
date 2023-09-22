cead1c2e75389b5b955f62bd409604347629f535
luka2210 <lukamomcilovic221099@gmail.com>
2023-09-22 12:40:05 +0200

    created model for frontend views

--- FullStack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
+++ FullStack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,4 +1,6 @@
+import { NONE_TYPE } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-employees-list',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesListComponent implements OnInit {
 
+  staticEmployee1: Employee = {
+    id: "sdasdasdjkashdaskdqid",
+    name: "Momcilo",
+    email: "moco123@gmail.com",
+    department: "based",
+    phone: 225883,
+    salary: 700
+  }
+
+  staticEmployee2: Employee = {
+    id: "egjkaskdieijfincad",
+    name: "Kalu",
+    email: "kalu123@gmail.com",
+    department: "based",
+    phone: 245365,
+    salary: 344
+  }
+
+  staticEmployee3: Employee = {
+    id: "asdjksnascadknvisd",
+    name: "Kalulu",
+    email: "kalulu@gmail.com",
+    department: "based",
+    phone: 254313,
+    salary: 720
+  }
+
+  employees: Employee[] = [this.staticEmployee1, this.staticEmployee2, this.staticEmployee3]
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
---
+++ FullStack UI/FullStack.UI/src/app/models/employee.model.ts
@@ -0,0 +1,8 @@
+export interface Employee {
+    id: String,
+    name: String,
+    email: String,
+    phone: Number,
+    salary: Number,
+    department: String
+}
