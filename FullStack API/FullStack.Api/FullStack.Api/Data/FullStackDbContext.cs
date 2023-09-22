724a2cf5ab8e06aa0be33d1563615a80ac87d200
luka2210 <lukamomcilovic221099@gmail.com>
2023-09-22 15:10:30 +0200

    added asp.net backedn

---
+++ FullStack API/FullStack.Api/FullStack.Api/Data/FullStackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FullStack.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.Api.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ FullStack API/FullStack.Api/FullStack.Api/Program.cs
@@ -0,0 +1,41 @@
+using FullStack.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// Connect to database
+string connectionString = builder.Configuration.GetConnectionString("FullStackConnectionString");
+MySqlServerVersion serverVersion = new MySqlServerVersion(new Version(8, 0, 32));
+
+builder.Services.AddDbContext<FullStackDbContext>(
+    dbContextOptions => dbContextOptions
+        .UseMySql(connectionString, serverVersion)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors()
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
